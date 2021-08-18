407d66f02bb6681e611ec2f7cc350621f877feb9
Ashutosh <vu4f1920027@pvppcoe.ac.in>
2021-08-19 01:23:09 +0530

    added 2 sections

--- main.js
+++ main.js
@@ -26,3 +26,45 @@ $(document).ready(function() {
     });
 
 });
+
+$(document).ready(function() {
+
+    $('.items').slick({
+        dots: false,
+        infinite: true,
+        speed: 800,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        responsive: [{
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: false,
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: false,
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: false,
+                }
+            }
+
+        ]
+    });
+});
